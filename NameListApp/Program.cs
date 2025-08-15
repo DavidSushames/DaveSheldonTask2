1e16d94afc57b8902cedc5f85df03b083e8263f2
DavidSushames <david.sushames@gmail.com>
2025-08-16 10:15:28 +1200

    Made an empty VisualStudio file

---
+++ NameListApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NameListApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This app will load a list of names from a .txt file into the listbox when a button is pressed.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
