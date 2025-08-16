a7b5e39d213dd76a936614f4e1d44deb2e0c7380
DavidSushames <david.sushames@gmail.com>
2025-08-16 13:01:11 +1200

    Added a button and listbox to Form1

--- NameListApp/Form1.Designer.cs
+++ NameListApp/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lbxNames = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(342, 53);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lbxNames
+            // 
+            this.lbxNames.FormattingEnabled = true;
+            this.lbxNames.Location = new System.Drawing.Point(288, 101);
+            this.lbxNames.Name = "lbxNames";
+            this.lbxNames.Size = new System.Drawing.Size(192, 264);
+            this.lbxNames.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxNames);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListBox lbxNames;
     }
 }
 
--- NameListApp/Form1.cs
+++ NameListApp/Form1.cs
@@ -16,5 +16,10 @@ namespace NameListApp
         {
             InitializeComponent();
         }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
